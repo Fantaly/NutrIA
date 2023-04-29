33f3bf0ad83f171d7ea86188fbdf59ee2c028f59
EmanuelRagusi <emanuel.ragusi@aiknowyou.ai>
2023-04-29 21:37:06 +0200

    Fixed demo import

--- src/views/Demo.js
+++ src/views/Demo.js
@@ -2,12 +2,12 @@ import React from "react";
 import Header from "../components/demo/header/header"
 import Login from "../components/demo/routes/login/login"
 import Form from "../components/demo/routes/nutritional_form/form";
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Selection from "../components/demo/routes/selection/selection";
+
 function Demo(){
     return (
         <>
-            
             <Header/>
             <Routes>
                 <Route index element={<Login />} />
